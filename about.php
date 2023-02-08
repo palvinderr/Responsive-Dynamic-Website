9ace007fa03cb818a06fc88e2e8acc523436519e
Palvinder Singh <9palvinder@gmail.com>
2023-02-08 12:01:50 +0900

    Initial commit

---
+++ about.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap" rel="stylesheet">
+
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">シンパルビンデル</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="about.php">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+        
+         
+        
+      </ul>
+      <form class="form-inline my-2 my-lg-0>
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    
+  </div>
+</nav>
+
+<div class="jumbotron">
+  <h1>シンTECH</h1>
+  <p>We are Web Developers, thank you for your faith.</p>
+</div>
+
+
+
+<section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center">About Us</h2>
+  </div>
+  <div class="container-fluids">
+   <div class="row">
+    <div class="col-lg-6 col-md-6 col-12">
+      <img class="card-img-top" src="images/11.png" class="img-fluid aboutimg">
+    </div>
+      <div class="col-lg-6 col-md-6 col-12">
+      <h2 class="display-4">私はシン パルビンデルです。</h2>
+      <p class="py-3">私の夢は IT エンジニアになることで、そのために自分の分野について日々新しいことを学んでいます。よろしくお願いいたします。</p>
+      <a href="about.php" class="btn btn-success"> check More</a>
+    </div>
+   </div>
+  </div>
+</section>
+    
+
+<footer>
+  <p class="p-3 bg-dark text-white text-center">@WarviTechProduction</p>
+</footer>
+
+
+
+
+</body>
+</html>
---
+++ inde.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap" rel="stylesheet">
+
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">シンパルビンデル</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="about.php">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+        
+         
+        
+      </ul>
+      <form class="form-inline my-2 my-lg-0>
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/a.png" alt="九州" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>九州</h3>
+        <p>自然!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/t.jpg" alt="東京" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>東京</h3>
+        <p>Thank you, 東京!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/f.jpg" alt="福岡" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>福岡</h3>
+        <p>地震がない!</p>
+      </div>   
+    </div>
+  </div>
+
+
+  </div>
+</nav>
+
+
+
+
+
+
+
+    <div class="card-body">
+          <h4 class="card-title">Beautiful Nature :)</h4>
+          <a href="#" class="btn btn-primary">See Profile</a>
+        </div>
+       </div> 
+
+    </div>
+
+    <div class="col-lg-6 col-md-6 col-12">
+      <div class="card">
+      <img class="card-img-top" src="images/11.png" alt="Card image">
+        <div class="card-body">
+    
